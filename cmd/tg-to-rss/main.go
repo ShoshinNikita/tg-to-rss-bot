5a2ba6e434e1ca0141a68ea561a5623875697678
Shoshin Nikita <shoshin.nikita@gmail.com>
2018-09-04 10:08:11 +0300

    Update package main

--- cmd/tg-to-rss/main.go
+++ cmd/tg-to-rss/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"github.com/ShoshinNikita/log"
+
+	"github.com/ShoshinNikita/tg-to-rss-bot/internal/bot"
+	"github.com/ShoshinNikita/tg-to-rss-bot/internal/rss"
+)
+
 func main() {
+	err := bot.Init()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = rss.Init()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	c := make(chan struct{})
+	<-c
 }
