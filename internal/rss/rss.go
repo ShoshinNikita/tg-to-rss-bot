b10fe11462a210f550f6fa17f45d53a287f94b61
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-01-12 19:51:40 +0300

    Update rss package

--- internal/rss/rss.go
+++ internal/rss/rss.go
@@ -2,12 +2,11 @@ package rss
 
 import (
 	"encoding/json"
-	"net/http"
+	"io"
 	"os"
 	"sync"
 	"time"
 
-	"github.com/ShoshinNikita/log"
 	"github.com/gorilla/feeds"
 	"github.com/pkg/errors"
 
@@ -19,57 +18,61 @@ const (
 	rssFile   = "rss/rss.json"
 )
 
-var (
-	feed  = new(feeds.Feed)
-	mutex = new(sync.RWMutex)
-)
+type Feed struct {
+	feed  *feeds.Feed
+	mutex *sync.RWMutex
+}
 
-func Init() error {
-	err := os.MkdirAll(rssFolder, 0600)
+func NewFeed() *Feed {
+	return &Feed{
+		feed:  new(feeds.Feed),
+		mutex: new(sync.RWMutex),
+	}
+}
+
+func (feed *Feed) Init() error {
+	err := os.MkdirAll(rssFolder, 0666)
 	if err != nil {
-		return errors.Wrapf(err, "Can't create folder %s", rssFolder)
+		return errors.Wrapf(err, "can't create folder %s", rssFolder)
 	}
 
 	f, err := os.Open(rssFile)
 	if err != nil {
 		// Need to create a file
-		f, err := os.Create(rssFile)
+		f, err = os.Create(rssFile)
 		if err != nil {
 			return errors.Wrapf(err, "can't create a new file %s", rssFile)
 		}
 		defer f.Close()
-		feed = &feeds.Feed{
+
+		feed.feed = &feeds.Feed{
 			Title:  "tg-to-rss-bot",
 			Link:   &feeds.Link{Href: "github.com/ShoshinNikita/tg-to-rss-bot"},
 			Author: &feeds.Author{Name: "ShoshinNikita"},
 		}
 
-		err = feed.WriteJSON(f)
+		err = feed.feed.WriteJSON(f)
 		if err != nil {
 			return errors.Wrap(err, "can't write RSS feed into file")
 		}
-
-		go runServer()
 
 		return nil
 	}
 	defer f.Close()
 
-	err = json.NewDecoder(f).Decode(feed)
+	err = json.NewDecoder(f).Decode(feed.feed)
 	if err != nil {
 		return errors.Wrap(err, "can't decode RSS feed from a file")
 	}
 
-	go runServer()
-
 	return nil
 }
 
-func Add(author, title, description, link string, created time.Time) error {
-	mutex.Lock()
-	defer mutex.Unlock()
+func (feed *Feed) Add(author, title, description, link string, created time.Time) error {
+	feed.mutex.Lock()
+	defer feed.mutex.Unlock()
 
-	feed.Add(&feeds.Item{
+	feed.feed.Add(&feeds.Item{
 		Author:      &feeds.Author{Name: author},
 		Created:     created,
 		Description: description,
@@ -77,13 +80,14 @@ func Add(author, title, description, link string, created time.Time) error {
 		Link:        &feeds.Link{Href: params.Host + "/" + link},
 	})
 
-	// Backup
-	f, err := os.OpenFile(rssFile, os.O_RDWR|os.O_TRUNC, 0600)
+	// Write into disk
+	f, err := os.OpenFile(rssFile, os.O_RDWR|os.O_TRUNC, 0666)
 	if err != nil {
 		return errors.Wrapf(err, "can't open file %s", rssFile)
 	}
 	defer f.Close()
-	err = feed.WriteJSON(f)
+
+	err = feed.feed.WriteJSON(f)
 	if err != nil {
 		return errors.Wrap(err, "can't write RSS feed into file")
 	}
@@ -91,20 +95,14 @@ func Add(author, title, description, link string, created time.Time) error {
 	return nil
 }
 
-func runServer() {
-	http.HandleFunc("/rss", serveRSS)
-	http.Handle("/data/", http.StripPrefix("/data/", http.FileServer(http.Dir("data/"))))
-	if err := http.ListenAndServe(":80", nil); err != nil {
-		log.Fatalf("Server crashed: %s\n", err)
-	}
-}
+func (feed *Feed) Write(w io.Writer) error {
+	feed.mutex.RLock()
+	defer feed.mutex.RUnlock()
 
-func serveRSS(w http.ResponseWriter, r *http.Request) {
-	mutex.RLock()
-	defer mutex.RUnlock()
-
-	err := feed.WriteRss(w)
+	err := feed.feed.WriteRss(w)
 	if err != nil {
-		log.Errorf("can't write RSS feed into http.ResponseWriter: %s", err)
+		errors.Wrap(err, "can't write RSS feed into io.Writer")
 	}
+
+	return nil
 }
